d13814361c0ca43440abaf7f8762911fcad6b796
liangkangnan <liangkangnan@163.com>
2024-08-26 15:56:29 +0800

    test:fpga: update hello_world
    Signed-off-by: liangkangnan <liangkangnan@163.com>

--- test/fpga/hello_world/main.c
+++ test/fpga/hello_world/main.c
@@ -1,14 +1,16 @@
 #include <stdint.h>
 
 #include "uart_drv.h"
-
+#include "xprintf.h"
 
 
 int main()
 {
     uart_init(115200);
+    xdev_out(uart_putc);
 
-    uart_puts("hello world!\n");
+    //uart_puts("hello world!\n");
+    xprintf("hellow!!!\n");
 
     while (1);
 }
