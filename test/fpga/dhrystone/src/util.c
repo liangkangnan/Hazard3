bd176ea88d64324af0b52305c18f1cf63eaab66e
liangkangnan <liangkangnan@163.com>
2024-08-27 11:35:19 +0800

    test:fpga: add dhrystone
    Signed-off-by: liangkangnan <liangkangnan@163.com>

---
+++ test/fpga/dhrystone/src/util.c
@@ -0,0 +1,5 @@
+#include "util.h"
+#include "uart_drv.h"
+#include "xprintf.h"
+
+
---
+++ test/fpga/dhrystone/src/util.h
@@ -0,0 +1,18 @@
+#ifndef _UTIL_H
+#define _UTIL_H
+
+#include <stdint.h>
+#include "uart_drv.h"
+#include "xprintf.h"
+
+#define setStats(x)
+
+#define read_csr(csrname) ({ \
+	uint32_t __csr_tmp_u32; \
+	__asm__ volatile ("csrr %0, " #csrname : "=r" (__csr_tmp_u32)); \
+	__csr_tmp_u32; \
+})
+
+#define debug_printf xprintf
+
+#endif
