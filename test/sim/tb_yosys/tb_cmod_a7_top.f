39cd3297788c61f85df42ae58abdbb12c96de440
liangkangnan <liangkangnan@163.com>
2024-09-20 09:55:15 +0800

    sim:tb_yosys: add cmod a7 test
    Signed-off-by: liangkangnan <liangkangnan@163.com>

---
+++ test/sim/tb_yosys/tb_cmod_a7_top.f
@@ -0,0 +1,4 @@
+file tb_cmod_a7_top.v
+file uart_sim.v
+
+list $HDL/../example_soc/synth/vivado/artix-7/cmod-a7/fpga_cmod_a7_top.f
---
+++ test/sim/tb_yosys/tb_cmod_a7_top.v
@@ -0,0 +1,25 @@
+// 封装实际的顶层模块，因此仿真的顶层模块名对外统一为tb_top
+
+module tb_top (
+	input wire        clk,
+	input wire        rst_n,
+
+	input  wire       tck,
+	input  wire       tms,
+	input  wire       tdi,
+	output wire       tdo,
+
+	output wire       led
+);
+
+    // 实际顶层模块
+    fpga_cmod_a7_top u_fpga (
+        .clk_12m(clk),
+        .tck(tck),
+        .tms(tms),
+        .tdi(tdi),
+        .tdo(tdo),
+        .led(led)
+    );
+
+endmodule
