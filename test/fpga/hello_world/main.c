6fef2402a4c17328d1ea0d6e0cf44e558cfe36bc
liangkangnan <liangkangnan@163.com>
2024-09-26 11:01:41 +0800

    test:fpga:hello: use printf
    Signed-off-by: liangkangnan <liangkangnan@163.com>

--- test/fpga/hello_world/main.c
+++ test/fpga/hello_world/main.c
@@ -1,16 +1,14 @@
 #include <stdint.h>
 
 #include "uart_drv.h"
-#include "xprintf.h"
+#include "printf.h"
 
 
 int main()
 {
     uart_init(115200);
-    xdev_out(uart_putc);
 
-    //uart_puts("hello world!\n");
-    xprintf("hellow!!!\n");
+    printf("hello hazard3!!!\n");
 
     while (1);
 }
