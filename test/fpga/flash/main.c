c0556b66fcc05d1dbefc6ee2755de904def3a929
liangkangnan <liangkangnan@163.com>
2024-10-16 11:46:38 +0800

    test:fpga:flash: fix when LEN == 256
    Signed-off-by: liangkangnan <liangkangnan@163.com>

--- test/fpga/flash/main.c
+++ test/fpga/flash/main.c
@@ -5,10 +5,10 @@
 #include "printf.h"
 #include "sim_ctrl.h"
 
-static uint8_t write_buffer[100];
-static uint8_t read_buffer[100];
+static uint8_t write_buffer[256];
+static uint8_t read_buffer[256];
 
-#define LEN 50
+#define LEN 256
 
 int main()
 {
@@ -18,13 +18,13 @@ int main()
     norflash_init();
     norflash_sector_erase(0x00);
 
-    for (uint8_t i = 0; i < LEN; i++)
+    for (uint32_t i = 0; i < LEN; i++)
         write_buffer[i] = i;
     norflash_page_write(0x00, write_buffer, LEN);
 
     norflash_read(0x00, read_buffer, LEN);
     uint8_t succ = 1;
-    for (uint8_t i = 0; i < LEN; i++) {
+    for (uint32_t i = 0; i < LEN; i++) {
         //printf("read buf[%d] = 0x%x\n", i, read_buffer[i]);
         if (read_buffer[i] != write_buffer[i]) {
             printf("error at %d\n", i);
