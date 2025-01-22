8f8ed33c660af0427778c4c9745dbd46d2e16d50
liangkangnan <liangkangnan@163.com>
2025-01-22 17:23:15 +0800

    fpga:driver:flash: run in dram zone
    Signed-off-by: liangkangnan <liangkangnan@163.com>

--- test/fpga/drivers/flash/inc/flash_drv.h
+++ test/fpga/drivers/flash/inc/flash_drv.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "spi_03h_xip_regs.h"
+#include "spi_qspi_xip_regs.h"
 
 #define FLASH_BASE_ADDR  (0x40008000)
 
@@ -12,15 +12,23 @@ typedef struct {
     volatile uint32_t CSR;
     volatile uint32_t TX;
     volatile uint32_t RX;
+    volatile uint32_t QSPI_CTRL;
+    volatile uint32_t DIV;
+    volatile uint32_t ICACHE_CTRL;
 } flash_t;
 
 #define FLASH ((flash_t *)FLASH_BASE_ADDR)
 
+void flash_set_clock_divider(uint8_t div);
 void flash_enter_cmd_mode();
 void flash_exit_cmd_mode();
 uint8_t flash_busy();
 void flash_set_cs(uint8_t level);
 void flash_write(uint8_t *buf, uint32_t len);
 void flash_read(uint8_t *buf, uint32_t len);
+void flash_set_qspi_mode_enabled(uint8_t en);
+void flash_set_qspi_dummy_count(uint8_t count);
+void flash_set_icache_enabled(uint8_t en);
+void flash_flush_icache_blocking();
 
 #endif
--- test/fpga/drivers/flash/src/flash_drv.c
+++ test/fpga/drivers/flash/src/flash_drv.c
@@ -1,16 +1,22 @@
 #include "flash_drv.h"
+#include "sections.h"
 
-void flash_enter_cmd_mode()
+void __not_in_flash_func(flash_set_clock_divider)(uint8_t div)
+{
+    FLASH->DIV = div;
+}
+
+void __not_in_flash_func(flash_enter_cmd_mode)()
 {
     FLASH->CSR |= 1 << XIP_CSR_DIRECT_LSB;
 }
 
-void flash_exit_cmd_mode()
+void __not_in_flash_func(flash_exit_cmd_mode)()
 {
     FLASH->CSR &= ~(1 << XIP_CSR_DIRECT_LSB);
 }
 
-uint8_t flash_busy()
+uint8_t __not_in_flash_func(flash_busy)()
 {
     if (FLASH->CSR & (1 << XIP_CSR_BUSY_LSB))
         return 1;
@@ -18,15 +24,15 @@ uint8_t flash_busy()
         return 0;
 }
 
-void flash_set_cs(uint8_t level)
+void __not_in_flash_func(flash_set_cs)(uint8_t level)
 {
     if (level)
-        flash_exit_cmd_mode();
+        FLASH->CSR |= 1 << XIP_CSR_CS_LEVEL_LSB;
     else
-        flash_enter_cmd_mode();
+        FLASH->CSR &= ~(1 << XIP_CSR_CS_LEVEL_LSB);
 }
 
-void flash_write(uint8_t *buf, uint32_t len)
+void __not_in_flash_func(flash_write)(uint8_t *buf, uint32_t len)
 {
     uint32_t i;
 
@@ -36,9 +42,11 @@ void flash_write(uint8_t *buf, uint32_t len)
         FLASH->TX = buf[i];
         while (flash_busy());
     }
+
+    while (flash_busy());
 }
 
-void flash_read(uint8_t *buf, uint32_t len)
+void __not_in_flash_func(flash_read)(uint8_t *buf, uint32_t len)
 {
     uint32_t i;
 
@@ -49,4 +57,35 @@ void flash_read(uint8_t *buf, uint32_t len)
         while (flash_busy());
         buf[i] = FLASH->RX;
     }
+
+    while (flash_busy());
+}
+
+void __not_in_flash_func(flash_set_qspi_mode_enabled)(uint8_t en)
+{
+    if (en)
+        FLASH->QSPI_CTRL |= 1 << XIP_QSPI_CTRL_MODE_LSB;
+    else
+        FLASH->QSPI_CTRL &= ~(1 << XIP_QSPI_CTRL_MODE_LSB);
+}
+
+void __not_in_flash_func(flash_set_qspi_dummy_count)(uint8_t count)
+{
+    FLASH->QSPI_CTRL &= ~XIP_QSPI_CTRL_DUMMY_MASK;
+    FLASH->QSPI_CTRL |= count << XIP_QSPI_CTRL_DUMMY_LSB;
+}
+
+void __not_in_flash_func(flash_set_icache_enabled)(uint8_t en)
+{
+    if (en)
+        FLASH->ICACHE_CTRL |= 1 << XIP_ICACHE_CTRL_EN_LSB;
+    else
+        FLASH->ICACHE_CTRL &= ~(1 << XIP_ICACHE_CTRL_EN_LSB);
+}
+
+void __not_in_flash_func(flash_flush_icache_blocking)()
+{
+    FLASH->ICACHE_CTRL |= 1 << XIP_ICACHE_CTRL_FLUSH_LSB;
+
+    while (FLASH->ICACHE_CTRL & (1 << XIP_ICACHE_CTRL_FLUSH_LSB));
 }
